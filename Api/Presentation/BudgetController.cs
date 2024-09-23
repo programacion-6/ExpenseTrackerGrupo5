91464bed657fe7a942d381c6ca0eb9b7a76a6508
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-23 11:19:45 -0400

    feat: budgets controller added based on the endpoints

---
+++ Api/Presentation/BudgetController.cs
@@ -0,0 +1,29 @@
+using Api.Domain;
+
+using AutoMapper;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/budgets")]
+public class BudgetController : ControllerBase
+{
+    private readonly IMapper _mapper;
+
+    public BudgetController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SetMonthlyBudget([FromBody] CreateBudgetRequest createBudgetRequest)
+    {
+        return Ok("Budget set successfully.");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCurrentMonthBudget()
+    {
+        return Ok("budget");
+    }
+}
