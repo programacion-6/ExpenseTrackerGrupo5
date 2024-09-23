8bd3b684d4b399d906f9bd2a870d4de1f2424419
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-23 11:20:22 -0400

    feat: goals controller added based on the endpoints

---
+++ Api/Presentation/GoalsController.cs
@@ -0,0 +1,47 @@
+using Api.Domain;
+
+using AutoMapper;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/goals")]
+public class GoalsController : ControllerBase
+{
+    private readonly IMapper _mapper;
+
+    public GoalsController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateGoal([FromBody] CreateGoalRequest createGoalRequest)
+    {
+        return Ok("Goal created successfully.");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllGoals()
+    {
+        return Ok("goals");
+    }
+
+    [HttpGet("actives")]
+    public async Task<IActionResult> GetActiveGoals()
+    {
+        return Ok("activeGoals");
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetGoalById(Guid id)
+    {
+        return Ok("goal");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteGoal(Guid id)
+    {
+        return Ok($"Goal with ID {id} deleted successfully.");
+    }
+}
