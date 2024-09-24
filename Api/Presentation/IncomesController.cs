c5f0b8f1f7d2bc39a77cb9776bb7dbd4436f9213
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-24 16:54:29 -0400

    chore: example inside a controller about how to get the user id when he or she is authenticated

--- Api/Presentation/IncomesController.cs
+++ Api/Presentation/IncomesController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+
 using Api.Domain;
 
 using AutoMapper;
@@ -21,7 +23,9 @@ public class IncomesController : ControllerBase
     [HttpPost]
     public async Task<IActionResult> LogIncome([FromBody] CreateIncomeRequest createIncomeRequest)
     {
-        return Ok("Income logged successfully.");
+        // This is an example of how to get the user id when he or she is authenticated
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        return Ok($"Income logged successfully for user with id: {userId}");
     }
 
     [HttpGet]
