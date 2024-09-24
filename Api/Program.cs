434e28ba2278c110dc90da7ccdb78b72cc1ee878
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-24 11:41:40 -0400

    feat: program configuration to inject dependencies and configuration for authentication

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,14 +1,38 @@
+using System.Data;
+
+using Api.Application;
+
+using Api.Domain;
+
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+/* var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
                        "Host=localhost;Database=expense_tracker_group_5_postgres;Username=user;Password=password";
 
-builder.Services.AddSingleton(new BaseContext(connectionString));
+builder.Services.AddSingleton(new BaseContext(connectionString)); */
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtConfiguration.ConfigAuthentication).AddJwtBearer(JwtConfiguration.ConfigBearer);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IHashingHandler, PasswordHashingHandler>();
+builder.Services.AddScoped<ITokenHandler, TokenHandler>();
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+builder.Services.AddTransient<IDbConnection>(connection =>
+    {
+        var conn = new NpgsqlConnection("Host=localhost;Database=expense_tracker_group_5_postgres;Username=user;Password=password");
+        conn.Open();
+        return conn;
+    });
 
 var app = builder.Build();
 
