d6d74fa0ebc2c9a5765def4a65a523e9e77fc349
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2024-09-24 14:47:22 -0400

    feat: add services in the program and conection

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,17 +1,29 @@
+using Npgsql;
 using System.Data;
-
 using Api.Application;
-
+using Api.Domain.Services;
+using Api.Domain;
 using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       "Host=localhost;Database=expense_tracker_group_5_postgres;Username=user;Password=password";
+
+builder.Services.AddTransient<IDbConnection>(sp => 
+    new NpgsqlConnection(connectionString));
+
 builder.Services.AddAuthorization();
 builder.Services.AddAuthentication(JwtConfiguration.ConfigAuthentication)
                 .AddJwtBearer(JwtConfiguration.ConfigBearer);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(SwaggerConfiguration.ConfigSwaggerGen);
+
 builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddScoped<IIncomeService, IncomeService>();
+builder.Services.AddScoped<IIncomeRepository, IncomeRepository>();
 builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation()
                 .AddFluentValidationClientsideAdapters();
