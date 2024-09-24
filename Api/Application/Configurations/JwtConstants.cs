e9fa28164b2989563028eda10eb7f83dc50ec58d
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-24 16:46:39 -0400

    chore: removed unncessary files

--- Api/Application/Configurations/JwtConstants.cs
+++
@@ -1,7 +0,0 @@
-namespace Api.Application;
-
-public static class JwtConstants
-{
-    public const string SecrectKey = "3fe876e048144285accbb83d4ee64c78";
-    public const uint ExpirationDays = 7;
-}
