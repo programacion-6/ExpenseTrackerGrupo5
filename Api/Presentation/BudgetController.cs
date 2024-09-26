e17165a3124976d8ea07c0c91e1aad2b4adc19d3
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-26 19:38:07 -0400

    chore: example how to use the email notifier service in a controller

--- Api/Presentation/BudgetController.cs
+++ Api/Presentation/BudgetController.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+
 using Api.Domain;
 
 using AutoMapper;
@@ -12,16 +14,38 @@ using Microsoft.AspNetCore.Mvc;
 public class BudgetController : ControllerBase
 {
     private readonly IMapper _mapper;
+    private readonly INotifier<EmailContent> _emailNotifier;
 
-    public BudgetController(IMapper mapper)
+    public BudgetController(IMapper mapper, INotifier<EmailContent> emailNotifier)
     {
         _mapper = mapper;
+        _emailNotifier = emailNotifier;
     }
 
     [HttpPost]
     public async Task<IActionResult> SetMonthlyBudget([FromBody] CreateBudgetRequest createBudgetRequest)
     {
-        return Ok("Budget set successfully.");
+        try
+        {
+            /* 
+            This is just a test of how to use the email service,
+            this code have to be removed
+             */
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+            var subjectEmail = "Subject prove";
+            var subjectBody = $"Body prove {createBudgetRequest.Currency}";
+            var email = new EmailContent(userEmail, subjectEmail, subjectBody);
+            _emailNotifier.Notify(email);
+            return Ok("Email sent");
+        }
+        catch (EmailNofiticationException exception)
+        {
+            return StatusCode(500, exception.Message);
+        }
+        catch (Exception exception)
+        {
+            return BadRequest(exception.Message);
+        }
     }
 
     [HttpGet]
