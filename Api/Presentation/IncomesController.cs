961ad943e3fe143d8fc6abbeeece6002755c01c1
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-28 17:49:04 -0400

    feat: tracking the current user budget when a new income is logged

--- Api/Presentation/IncomesController.cs
+++ Api/Presentation/IncomesController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 using Api.Domain.Services;
+using Api.Application;
 
 [ApiController]
 [Route("api/incomes")]
@@ -17,29 +18,54 @@ public class IncomesController : ControllerBase
 {
     private readonly IMapper _mapper;
     private readonly IIncomeService _incomeService;
+    private readonly BudgetManagement _budgetManagement;
 
-    public IncomesController(IIncomeService incomeService, IMapper mapper)
+    public IncomesController(IIncomeService incomeService, IMapper mapper, BudgetManagement budgetManagement)
     {
         _incomeService = incomeService;
         _mapper = mapper;
+        _budgetManagement = budgetManagement;
     }
 
     [HttpPost]
     public async Task<IActionResult> LogIncome([FromBody] CreateIncomeRequest createIncomeRequest)
     {
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var userEmail = User.FindFirstValue(ClaimTypes.Email);
+
+        if (userId is null || userEmail is null) {
+            return BadRequest("User not found");
+        }
 
         var income = _mapper.Map<Income>(createIncomeRequest);
         income.UserId = Guid.Parse(userId);
 
         var result = await _incomeService.CreateAsync(income);
 
-        if (result)
+        if (!result)
         {
-            return Ok($"Income logged successfully");
+            return StatusCode(500, "Error saving income.");
         }
-        
-        return StatusCode(500, "Error saving income.");
+
+        try
+        {
+            await _budgetManagement.ProcessNewIncome(income, userEmail);
+        }
+        catch (UserBudgetException exception)
+        {
+            return StatusCode(400, $"{exception.Message}");
+        }
+        catch (BudgetException exception)
+        {
+            return StatusCode(400, $"{exception.Message}");
+        }
+        catch (Exception exception)
+        {
+            return StatusCode(500, $"Internal server error: {exception.Message}");
+        }
+
+        return Ok($"Income logged successfully");
+
     }
 
 
@@ -59,19 +85,19 @@ public class IncomesController : ControllerBase
 
 
     [HttpGet("{id}")]
-public async Task<IActionResult> GetIncomeById(Guid id)
-{
-    var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-    var income = await _incomeService.GetByIdAsync(id);
-
-    if (income == null || income.UserId != Guid.Parse(userId))
+    public async Task<IActionResult> GetIncomeById(Guid id)
     {
-        return NotFound("Income not found or you do not have permission to access this income.");
-    }
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-    return Ok(income);
-}
+        var income = await _incomeService.GetByIdAsync(id);
+
+        if (income == null || income.UserId != Guid.Parse(userId))
+        {
+            return NotFound("Income not found or you do not have permission to access this income.");
+        }
+
+        return Ok(income);
+    }
 
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateIncome(Guid id, [FromBody] UpdateIncomeRequest updateIncomeRequest)
@@ -84,7 +110,7 @@ public async Task<IActionResult> GetIncomeById(Guid id)
         {
             return NotFound("Income not found or you do not have permission to update this income.");
         }
-        
+
         income.Currency = updateIncomeRequest.Currency;
         income.Source = updateIncomeRequest.Source;
         income.Amount = updateIncomeRequest.Amount;
@@ -109,7 +135,7 @@ public async Task<IActionResult> GetIncomeById(Guid id)
         {
             return NotFound("Income not found or you do not have permission to delete this income.");
         }
-        var result = await _incomeService.DeleteAsync(id); 
+        var result = await _incomeService.DeleteAsync(id);
         if (result)
         {
             return Ok("Income deleted successfully.");
