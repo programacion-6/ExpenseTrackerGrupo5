9aa82d676cab676b92e173b8a26d68c2bc3b5fa3
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-28 18:02:06 -0400

    feat: implemented behavior to set monthly budget and get the current budget based on the current month using the budget service

--- Api/Presentation/BudgetController.cs
+++ Api/Presentation/BudgetController.cs
@@ -20,23 +20,41 @@ public class BudgetController : ControllerBase
     {
         _mapper = mapper;
         _budgetService = budgetService;
-
     }
 
     [HttpPost]
     public async Task<IActionResult> SetMonthlyBudget([FromBody] CreateBudgetRequest createBudgetRequest)
     {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var userEmail = User.FindFirstValue(ClaimTypes.Email);
+
+        if (userEmail is null)
+        {
+            return BadRequest("User not found");
+        }
+
+        var newBudget = _mapper.Map<Budget>(createBudgetRequest);
+
         try
         {
+            await _budgetService.AddMonthlyUserBudget(newBudget);
             return Ok();
         }
-        catch (EmailNotificationException exception)
+        catch (BudgetException exception)
         {
-            return StatusCode(500, exception.Message);
+            return StatusCode(400, exception.Message);
+        }
+        catch (UserBudgetException exception)
+        {
+            return StatusCode(400, exception.Message);
         }
         catch (Exception exception)
         {
-            return BadRequest(exception.Message);
+            return StatusCode(500, $"Internal server error: {exception.Message}");
         }
     }
 
