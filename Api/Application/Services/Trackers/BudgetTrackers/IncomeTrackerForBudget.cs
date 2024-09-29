ea0dd3e1c6250f3a02e0f7cc0df0f9cc9edebe3f
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-28 23:32:33 -0400

    feat: implemented income tracker for budgets

---
+++ Api/Application/Services/Trackers/BudgetTrackers/IncomeTrackerForBudget.cs
@@ -0,0 +1,46 @@
+
+using Api.Domain;
+
+namespace Api.Application;
+
+public class IncomeTrackerForBudget : ITracker<Income, Budget>
+{
+    private readonly IBudgetService _budgetService;
+    private readonly INotifier<EmailContent> _notifier;
+
+    public IncomeTrackerForBudget(IBudgetService budgetService, INotifier<EmailContent> notifier)
+    {
+        _budgetService = budgetService;
+        _notifier = notifier;
+    }
+
+    public async Task TrackNewUserEntry(Income income, string userEmail)
+    {
+        var currentBudget = await _budgetService.GetCurrentUserBudget(income.UserId);
+        currentBudget.CurrentAmount += income.Amount;
+        await NotifyTrackingToUser(currentBudget, userEmail);
+    }
+
+    public async Task TrackUpdatedUserEntry(Income oldIncome, Income newIncome, string userEmail)
+    {
+        var currentBudget = await _budgetService.GetCurrentUserBudget(oldIncome.UserId);
+        currentBudget.CurrentAmount -= oldIncome.Amount;
+        currentBudget.CurrentAmount += newIncome.Amount;
+        await NotifyTrackingToUser(currentBudget, userEmail);
+    }
+
+    public async Task TrackDeletedUserEntry(Income income, string userEmail)
+    {
+        var currentBudget = await _budgetService.GetCurrentUserBudget(income.UserId);
+        currentBudget.CurrentAmount -= income.Amount;
+        await NotifyTrackingToUser(currentBudget, userEmail);
+    }
+
+    public async Task NotifyTrackingToUser(Budget budget, string userEmail)
+    {
+        await _budgetService.UpdateUserBudget(budget);
+        var budgetTracker = new TrackingNotifierChainClient(_notifier, userEmail);
+        await budgetTracker.Handle(budget);
+    }
+
+}
