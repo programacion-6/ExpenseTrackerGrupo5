c90e1997c0f01597f838b2bf71cbe94040af5003
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-28 23:31:21 -0400

    refactor: changed signature for tracking user entries

--- Api/Domain/Services/ITracker.cs
+++ Api/Domain/Services/ITracker.cs
@@ -1,6 +1,9 @@
 namespace Api.Domain;
 
-public interface ITracker<TTrackeable>
+public interface ITracker<TEntry, TTrackable>
 {
-    public Task Track(TTrackeable trackeable);
+    public Task TrackNewUserEntry(TEntry entry, string userEmail);
+    public Task TrackUpdatedUserEntry(TEntry oldEntry, TEntry newEntry, string userEmail);
+    public Task TrackDeletedUserEntry(TEntry entry, string userEmail);
+    public Task NotifyTrackingToUser(TTrackable trackable, string userEmail);
 }
