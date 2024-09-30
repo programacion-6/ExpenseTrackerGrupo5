9481654ab42654736b7c69da00784bd6c63363be
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-29 22:49:26 -0400

    feat: improved the way to tracking budget by months when an income has been updated or deleted

--- Api/Application/Services/Trackers/BudgetTrackers/IncomeTrackerForBudget.cs
+++ Api/Application/Services/Trackers/BudgetTrackers/IncomeTrackerForBudget.cs
@@ -23,24 +23,50 @@ public class IncomeTrackerForBudget : ITracker<Income, Budget>
 
     public async Task TrackUpdatedUserEntry(Income oldIncome, Income newIncome, string userEmail)
     {
-        var currentBudget = await _budgetService.GetCurrentUserBudget(oldIncome.UserId);
-        currentBudget.CurrentAmount -= oldIncome.Amount;
-        currentBudget.CurrentAmount += newIncome.Amount;
-        await NotifyTrackingToUser(currentBudget, userEmail);
+        if (DateChecker.AreSameDate(oldIncome.Date, newIncome.Date))
+        {
+            await TrackUpdatedIncomeWithSameDate(oldIncome, newIncome, userEmail);
+        }
+        else
+        {
+            await TrackUpdatedIncomeWithDifferentDate(oldIncome, newIncome, userEmail);
+        }
+    }
+
+    private async Task TrackUpdatedIncomeWithSameDate(Income oldIncome, Income newIncome, string userEmail)
+    {
+        var budget = await _budgetService.GetUserBudgetByMonthOrCreate(oldIncome.UserId, oldIncome.Date);
+        budget.CurrentAmount -= oldIncome.Amount;
+        budget.CurrentAmount += newIncome.Amount;
+        await NotifyTrackingToUser(budget, userEmail);
+    }
+
+    private async Task TrackUpdatedIncomeWithDifferentDate(Income oldIncome, Income newIncome, string userEmail)
+    {
+        var oldBudget = await _budgetService.GetUserBudgetByMonthOrCreate(oldIncome.UserId, oldIncome.Date);
+        var newBudget = await _budgetService.GetUserBudgetByMonthOrCreate(newIncome.UserId, newIncome.Date);
+        oldBudget.CurrentAmount -= oldIncome.Amount;
+        newBudget.CurrentAmount += newIncome.Amount;
+        await NotifyTrackingToUser(oldBudget, userEmail);
+        await NotifyTrackingToUser(newBudget, userEmail);
     }
 
     public async Task TrackDeletedUserEntry(Income income, string userEmail)
     {
-        var currentBudget = await _budgetService.GetCurrentUserBudget(income.UserId);
-        currentBudget.CurrentAmount -= income.Amount;
-        await NotifyTrackingToUser(currentBudget, userEmail);
+        var budget = await _budgetService.GetUserBudgetByMonthOrCreate(income.UserId, income.Date);
+        budget.CurrentAmount -= income.Amount;
+        await NotifyTrackingToUser(budget, userEmail);
     }
 
     public async Task NotifyTrackingToUser(Budget budget, string userEmail)
     {
         await _budgetService.UpdateUserBudget(budget);
-        var budgetTracker = new TrackingNotifierChainClient(_notifier, userEmail);
-        budgetTracker.Handle(budget);
+
+        if (DateChecker.IsCurrent(budget.Month))
+        {
+            var budgetTracker = new TrackingNotifierChainClient(_notifier, userEmail);
+            budgetTracker.Handle(budget);
+        }
     }
 
 }
